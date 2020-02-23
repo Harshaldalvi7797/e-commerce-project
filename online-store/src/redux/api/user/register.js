cd2aa75f94678bb81d21aff5967c6579ad260a10
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-02-23 19:26:58 +0530

    commit : remove outeside npm

---
+++ online-store/src/redux/api/user/register.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+const REGISTER_ENDPOINT = "http://localhost:4600/api/createuser";
+let config = {
+  headers: {
+    "content-type": "application/json"
+  }
+};
+
+export const userRegister = data => {
+  return axios.post(REGISTER_ENDPOINT, JSON.stringify(data), config);
+};
